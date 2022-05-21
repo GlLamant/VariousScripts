1271f03c30a8aab2da855e9ce388ffae7833cfda
Lay <guoleilam@163.com>
2022-05-21 11:48:31 +0800

    feat: 新增foundation.sh，定义自有函数

---
+++ bash/foundation/foundation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This file import all my basic function scripts
+
+<< lay_instruction
+    @brief echo a new line
+    @return null
+lay_instruction
+lay_fn_new_line() {
+    echo 
+}

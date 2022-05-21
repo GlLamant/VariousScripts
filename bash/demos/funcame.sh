11dd81aa2cd16f98de168329b806b8169691cb09
Lay <guoleilam@163.com>
2022-05-21 16:22:59 +0800

    update: demo/funcname.sh 1. 打印完成调用链

--- bash/demos/funcame.sh
+++ bash/demos/funcame.sh
@@ -6,10 +6,20 @@ function fn_a() {
     # echo ${FUNCNAME[0]}           # ${FUNCNAME[0]}表示当前函数名称
     # echo ${FUNCNAME[1]}           # ${FUNCNAME[1]}表示调用当前函数的函数名称
     func_size=${#FUNCNAME}
+    func_chain=""
     for ((i=${func_size}-1; i>=0; i--)) 
     do
+        cur_func_name=${FUNCNAME[${i}]}
+        if test -z ${func_chain}; then
+            func_chain="${cur_func_name}"
+        else
+            func_chain="${func_chain}->${cur_func_name}"
+        fi
         echo ${FUNCNAME[${i}]}
     done;
+
+    echo 
+    echo func_chain: ${func_chain}
 }
 
 function fn_b() {
